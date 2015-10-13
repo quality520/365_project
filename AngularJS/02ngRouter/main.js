5fefc108b7ba6d637d4f02a70d23bc1754b63d64
quality520 <silencly520@163.com>
2015-10-13 17:38:48 +0800

    ngRoute finish

--- AngularJS/02ngRouter/main.js
+++ AngularJS/02ngRouter/main.js
@@ -2,17 +2,17 @@ angular.module("myApp",["ngRoute"])
 
 .config(["$routeProvider",function($routeProvider){
      $routeProvider
-         .when('1',{
-             templateUrl:'/1.html',
-             controller:"mainCtrl"
+         .when('/1',{
+             templateUrl:'test/1.html'
+
          })
-         .when('2',{
-             templateUrl:'/2.html',
-             controller:"mainCtrl"
+         .when('/2',{
+             templateUrl:'test/2.html'
+
          })
-         .when('3',{
-             templateUrl:'/3.html',
-             controller:"mainCtrl"
+         .when('/3',{
+             templateUrl:'test/3.html'
+
          })
          .when('/',{
              templateUrl:'/index.html',
@@ -21,18 +21,10 @@ angular.module("myApp",["ngRoute"])
          .otherwise({
              redirectTo:'/'
          });
+
 }])
-    .controller("mainCtrl",function($scope){
+    .controller("mainCtrl",['$scope',function($scope){
 
-    })
-   /* .controller("index1Ctrl",function($scope){
-
-    })
-    .controller("index2Ctrl",function($scope){
-
-    })
-    .controller("index3Ctrl",function($scope){
-
-    })*/
+    }])
 
 
